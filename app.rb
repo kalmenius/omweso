05d7f1183e2992b01f4a680130db8ef54d705eb8
Jeremy P. Wright <jeremy@kalmenius.com>
2019-01-03 13:59:03 -0500

    /info endpoint and simple error handling

--- app.rb
+++ app.rb
@@ -1,10 +1,32 @@
 require 'sinatra'
 
+# Adjust global Sinatra settings.
+configure do
+	set :show_exceptions, false
+end
+
+# JSON-ify all responses.
 after do
 	content_type :json
 	body body.to_json
 end
 
-get '*' do
-	{:meep => "moop"}
+# Handle bad routes nicely.
+not_found do
+	{error: "Route not found: #{request.fullpath}"}
+end
+
+# Handle uncaught exceptions nicely.
+error do
+	{error: env['sinatra.error'].inspect}
+end
+
+# An endpoint to inspect application state externally.
+get '/info' do
+	{environment: Sinatra::Application.environment, sha: env['SOURCE_VERSION']}
+end
+
+# An endpoint just for the tests.
+get '/error' do
+	xyzzy.inspect
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,11 +1,13 @@
 describe "Omweso" do
-	it "should produce a known JSON on all routes" do
-		get '/'
+	it "should respond to info requests" do
+		get '/info'
 		expect(last_response).to be_ok
-		expect(last_response.body).to eq("{\"meep\":\"moop\"}")
+		expect(JSON.parse(last_response.body)["environment"]).to eq("test")
 	end
 	
-	it "should not fail this test" do
-		# fail
+	it "should handle bad routes nicely" do
+		get '/bad-route?foo=bar'
+		expect(last_response).to be_not_found
+		expect(last_response.body).to eq("{\"error\":\"Route not found: /bad-route?foo=bar\"}")
 	end
 end
