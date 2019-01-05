eae1185f234ce90522444b438e0593269a2d7491
Jeremy P. Wright <jeremy@kalmenius.com>
2019-01-05 12:05:11 -0500

    spec cleanup, adding config.yml

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
+require 'sinatra/config_file'
+
+config_file 'config.yml'
 
 # Adjust global Sinatra settings.
 configure do
 	set :show_exceptions, false
+	set :raise_errors, false
+end
+
+# Adjust test-environment-only settings.
+configure :test do
+	get '/error' do
+		raise "xyzzy"
+	end
 end
 
 # JSON-ify all responses.
@@ -22,6 +33,7 @@ error do
 end
 
 # An endpoint to inspect application state externally.
-get '/info' do
-	{environment: Sinatra::Application.environment, sha: ENV['HEROKU_SLUG_COMMIT']}
-end
+get '/info' do {
+	environment: Sinatra::Application.environment,
+	sha: ENV['HEROKU_SLUG_COMMIT']
+} end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,13 +1,50 @@
 describe "Omweso" do
-	it "should respond to info requests" do
-		get '/info'
-		expect(last_response).to be_ok
-		expect(JSON.parse(last_response.body)["environment"]).to eq("test")
+	after do
+		expect(last_response.content_type).to eq("application/json")
 	end
 	
-	it "should handle bad routes nicely" do
-		get '/bad-route?foo=bar'
-		expect(last_response).to be_not_found
-		expect(last_response.body).to eq("{\"error\":\"Route not found: /bad-route?foo=bar\"}")
+	context "should handle info requests" do
+		before do
+			get '/info'
+			@json = JSON.parse(last_response.body)
+		end
+		
+		it "with HTTP 200" do
+			expect(last_response.status).to eq(200)
+		end
+		
+		it "with the Sinatra environment" do
+			expect(@json["environment"]).to eq("test")
+		end
+	end
+	
+	context "should handle bad routes" do
+		before do
+			get '/bad-route?foo=bar'
+			@json = JSON.parse(last_response.body)
+		end
+		
+		it "with HTTP 404" do
+			expect(last_response.status).to eq(404)
+		end
+		
+		it "with some custom error text" do
+			expect(@json["error"]).to eq("Route not found: /bad-route?foo=bar")
+		end
+	end
+	
+	context "should handle uncaught exceptions" do
+		before do
+			get '/error'
+			@json = JSON.parse(last_response.body)
+		end
+		
+		it "with HTTP 500" do
+			expect(last_response.status).to eq(500)
+		end
+		
+		it "with some exception information" do
+			expect(@json["error"]).to eq("#<RuntimeError: xyzzy>")
+		end
 	end
 end
