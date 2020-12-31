374ee187ff7e22c9c0669c48c68b9e69e73cdab5
Jeremy P. Wright <jeremy@kalmenius.com>
2020-12-31 13:23:03 -0500

    endless endless

--- backend/models/game.rb
+++ backend/models/game.rb
@@ -7,16 +7,12 @@ class Game < Sequel::Model
   plugin :timestamps, update_on_create: true
   plugin :touch
 
-  def self.[](id)
-    super(id) || (raise NotFound, id)
-  end
+  def self.[](id) = super(id) || (raise NotFound, id)
 
   # An error indicating that the game with the given ID isn't present in the database.
   class NotFound < StandardError
-    include HTTP404
+    def http_status = 404
 
-    def initialize(id)
-      super "Game #{id} not found!"
-    end
+    def initialize(id) = super("Game #{id} not found!")
   end
 end
